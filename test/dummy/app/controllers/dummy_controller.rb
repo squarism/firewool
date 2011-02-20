1ef933b253afb2ce91721a4e7e6f6db9ebfc0bb3
Chris Dillon <milkfilk@gmail.com>
2011-02-20 11:38:42 -0500

    Dummy routes for testing.

--- test/dummy/app/controllers/dummy_controller.rb
+++ test/dummy/app/controllers/dummy_controller.rb
@@ -2,4 +2,8 @@ class DummyController < ApplicationController
   include Firewool
   acts_as_firewalled
   before_filter :ip_filter
+  
+  def index
+    render :text => "This is the index method on DummyController"
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -5,6 +5,7 @@ Dummy::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'dummy' => 'dummy#index'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
