71958640f91e9cc231d4fded5bbed9dff24a2af0
Chris Dillon <milkfilk@gmail.com>
2011-02-21 21:58:38 -0500

    Comment cleanup.

--- lib/firewool.rb
+++ lib/firewool.rb
@@ -14,9 +14,8 @@ module Firewool
   class Config
     include Singleton
     attr_reader :yaml_config
-    attr_accessor :filesystem_hits
+    attr_accessor :filesystem_hits  # track how many times we read the yml
     
-    # just here for debugging filesystem hits
     def load_config
       @filesystem_hits += 1
       YAML.load_file("#{Rails.root.to_s}/config/firewool.yml")
@@ -26,16 +25,9 @@ module Firewool
       @@yaml_config ||= self.load_config
     end
     
-    # TODO: Ugh, globals?  Really?  How can I cache the config in case someone
-    # has a million controllers that include Firewool?  This gets hit every include Module.
     def initialize
       @filesystem_hits = 0
-    #   if $firewool_config.nil?
-    #     @yaml_config = YAML.load_file("#{Rails.root.to_s}/config/firewool.yml")
-    #     $firewool_config = @yaml_config
-    #   end      
     end
-    
   end
 
   autoload :Hook, File.join(File.dirname(__FILE__), "firewool/hook")
